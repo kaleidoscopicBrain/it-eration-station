ae8d4379022eea11d961b9fdadbc33b965d8f033
kaleidoscopicBrain <45481462+kaleidoscopicBrain@users.noreply.github.com>
2025-01-27 14:49:27 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
@@ -15,21 +15,21 @@ def home():
 def set_name():
     global ceo_name
     ceo_name = request.form['player_name']
-    return redirect(url_for('home'))
+    return redirect('/')
 
 @app.route('/write_code', methods=['POST'])
 def write_code():
     global money, tasks_completed
     money += 50
     tasks_completed += 1
-    return redirect(url_for('home'))
+    return jsonify(render_template('game_info.html', money=money, tasks_completed=tasks_completed))
 
 @app.route('/solve_ticket', methods=['POST'])
 def solve_ticket():
     global money, tasks_completed
     money += 30
     tasks_completed += 1
-    return redirect(url_for('home'))
+    return jsonify(render_template('game_info.html', money=money, tasks_completed=tasks_completed))
 
 if __name__ == '__main__':
     app.run(debug=True)
